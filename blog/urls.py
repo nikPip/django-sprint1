f6b0934862a39c3b5f5d23e652a31edabd3bb73c
Nikita Gorbov <nik.gorbov.02@mail.ru>
2025-09-03 14:15:05 +0300

    Исправление оставшихся ошибок PEP8 в blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 app_name = 'blog'
 
-urlpatterns = [ 
+urlpatterns = [
     path('', views.index, name='index'),
-    path('post/<int:id>', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts')
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
+    path('category/<slug:category_slug>/', views.category_posts,
+         name='category_posts'),
 ]
