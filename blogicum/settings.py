7074e19fc162d03fa80649bdb0cebb5562e55280
Nikita Gorbov <nik.gorbov.02@mail.ru>
2025-09-07 01:07:07 +0300

    pep8

--- blogicum/settings.py
+++ blogicum/settings.py
@@ -34,7 +34,7 @@ ROOT_URLCONF = 'blogicum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],  
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
